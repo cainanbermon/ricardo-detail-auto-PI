e4bb2fa0d51cf8f80e2420b91bca44d2369f1c3e
Bruno Righi <brunorighi267@gmail.com>
2024-11-18 19:21:41 -0300

    adicionando links na página de contato e acertando os ícones dos serviços da página home

--- js/contato.js
+++ js/contato.js
@@ -4,7 +4,7 @@ var lat = -22.26817;
 var lon = -48.55311;
 
 // Inicializar o mapa
-var mapa = L.map('mapa').setView([lat, lon], 15);
+var mapa = L.map('mapa').setView([lat, lon], 15.5);
 
 // Adicionar camada do OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapa);
@@ -27,7 +27,7 @@ var geocodeUrl = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${l
 // Usar fetch para obter o endereço
 fetch(geocodeUrl).then(response => response.json()).then(data => {
     var endereco = 'R. Júlio Carbone, 1009 - Jardim Sao Crispim';
-    var nome = '<b>Ricardo Detalhamento Automotivo</b>'
+    var link_maps = '<a href="https://www.google.com.br/maps/place/Ricardo+Detalhamento+Automotivo/@-22.2683204,-48.5557521,17z/data=!3m1!4b1!4m6!3m5!1s0x94c757fe6388fcd5:0x2c96f7a23d1f7e97!8m2!3d-22.2683254!4d-48.5531772!16s%2Fg%2F11fzf56x26?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"><b>Ricardo Detalhamento Automotivo</b></a>'
     icon_marcador.bindTooltip(endereco);
-    icon_marcador.bindPopup(nome).openPopup();
+    icon_marcador.bindPopup(link_maps).openPopup();
 }).catch(error => console.error('Erro ao obter o endereço:', error));
